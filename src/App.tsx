53bc1c931189ec98ea2d757859d562986917ab7d
Eakudompong Chanoknan <ceguantz96@outlook.com>
2023-10-04 12:26:17 +0900

    config layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
-import "./App.css";
-const banner = `
+const banner: string = `
 
-███████╗ ██████╗        ██████╗ ███████╗██╗   ██╗
-██╔════╝██╔════╝        ██╔══██╗██╔════╝██║   ██║
-█████╗  ██║             ██║  ██║█████╗  ██║   ██║
-██╔══╝  ██║             ██║  ██║██╔══╝  ╚██╗ ██╔╝
-███████╗╚██████╗███████╗██████╔╝███████╗ ╚████╔╝ 
-╚══════╝ ╚═════╝╚══════╝╚═════╝ ╚══════╝  ╚═══╝  
-                                                 
-`;
+
+███████╗ ██████╗       █████╗  ██████╗ ██████╗ ██╗  ██╗
+██╔════╝██╔════╝      ██╔══██╗██╔════╝ ╚════██╗██║  ██║
+█████╗  ██║     █████╗╚██████║███████╗  █████╔╝███████║
+██╔══╝  ██║     ╚════╝ ╚═══██║██╔═══██╗██╔═══╝ ╚════██║
+███████╗╚██████╗       █████╔╝╚██████╔╝███████╗     ██║
+╚══════╝ ╚═════╝       ╚════╝  ╚═════╝ ╚══════╝     ╚═╝
+                                                       
+
+Type 'help' to see the list of available commands.
+Type 'sumfetch' to display summary.
+Type 'repo' or click `;
 function App() {
   return (
     <>
-      <pre>{banner}</pre>
+      <div className="p-8 overflow-hidden h-full border-2 rounded ">
+        <pre>{banner}</pre>
+        <p className="text-green-500">visitor@EC-9624:$ ~ </p>
+        <input type="text" name="input" id="input" />
+      </div>
     </>
   );
 }
