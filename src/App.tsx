3d669532a6a7c25741b9d2693e524802eeb969ff
Eakudompong Chanoknan <ceguantz96@outlook.com>
2023-10-05 16:44:32 +0900

    add basic comands template

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,7 @@
-const banner: string = `
-
-
-███████╗ ██████╗       █████╗  ██████╗ ██████╗ ██╗  ██╗
-██╔════╝██╔════╝      ██╔══██╗██╔════╝ ╚════██╗██║  ██║
-█████╗  ██║     █████╗╚██████║███████╗  █████╔╝███████║
-██╔══╝  ██║     ╚════╝ ╚═══██║██╔═══██╗██╔═══╝ ╚════██║
-███████╗╚██████╗       █████╔╝╚██████╔╝███████╗     ██║
-╚══════╝ ╚═════╝       ╚════╝  ╚═════╝ ╚══════╝     ╚═╝
-                                                       
-
-Type 'help' to see the list of available commands.
-Type 'sumfetch' to display summary.
-Type 'repo' or click `;
 function App() {
   return (
     <>
       <div className="p-8 overflow-hidden h-full border-2 rounded ">
-        <pre>{banner}</pre>
         <p className="text-green-500">visitor@EC-9624:$ ~ </p>
         <input type="text" name="input" id="input" />
       </div>
---
+++ src/utils/commands.ts
@@ -0,0 +1,43 @@
+export const banner = (): string => {
+  return `
+    
+  ██████████   █████████              ████████   ████████   ████████  █████ █████ 
+  ░░███░░░░░█  ███░░░░░███            ███░░░░███ ███░░░░███ ███░░░░███░░███ ░░███  
+   ░███  █ ░  ███     ░░░            ░███   ░███░███   ░░░ ░░░    ░███ ░███  ░███ █
+   ░██████   ░███          ██████████░░█████████░█████████    ███████  ░███████████
+   ░███░░█   ░███         ░░░░░░░░░░  ░░░░░░░███░███░░░░███  ███░░░░   ░░░░░░░███░█
+   ░███ ░   █░░███     ███            ███   ░███░███   ░███ ███      █       ░███░ 
+   ██████████ ░░█████████            ░░████████ ░░████████ ░██████████       █████ 
+  ░░░░░░░░░░   ░░░░░░░░░              ░░░░░░░░   ░░░░░░░░  ░░░░░░░░░░       ░░░░░  
+                                                                                   
+                                                                                                                                                            
+    Type 'help' to see the list of available commands.
+    Type 'sumfetch' to display summary.
+    Type 'repo' or click  `;
+}; //DOS Rebel
+
+export const about = (): string => {
+  return `Hi, I am EC-9624. 
+      Welcome to my website!
+      More about me:
+      'sumfetch' - short summary.
+      'resume' - my latest resume.
+      'readme' - my github readme.`;
+};
+
+//Contact
+export const gitHub = (): string => {
+  return `Opening gitHub...`;
+};
+
+export const wantedly = (): string => {
+  return `Opening Wantedly...`;
+};
+
+export const linkedIn = (): string => {
+  return `Opening Linkedin...`;
+};
+
+export const email = (): string => {
+  return `email@email.com`;
+};
