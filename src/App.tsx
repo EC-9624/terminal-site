9104ba1f5b650175b22aeefebd32292c5de8e657
Eakudompong Chanoknan <ceguantz96@outlook.com>
2023-10-06 13:33:02 +0900

    add api

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { banner } from "./utils/commands";
 function App() {
   return (
     <>
-      <div className="overflow-hidden h-full border-2 rounded ">
-        <pre>{banner()}</pre>
-      </div>
+      <div className="overflow-hidden h-full border-2 rounded "></div>
     </>
   );
 }
---
+++ src/utils/apis.ts
@@ -0,0 +1,14 @@
+import config from '../../config.json';
+import axios from 'axios';
+
+export const getReadme = async () => {
+  const { data } = await axios.get(config.readmeUrl);
+  return data;
+};
+
+export const getProject = async () => {
+  const { data } = await axios.get(
+    `https://api.github.com/users/${config.social.github}/repos`,
+  );
+  return data;
+};
