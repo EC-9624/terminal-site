ba7d001809e8386c2bbdf45ee63a63367cc6e31d
Eakudompong Chanoknan <ceguantz96@outlook.com>
2023-11-09 20:47:31 +0900

    fix type in shell

--- src/utils/shell.ts
+++ src/utils/shell.ts
@@ -1,25 +1,29 @@
 import React from 'react';
 import * as cmds from './commands';
 
+type CommandFunctions = {
+  [key: string]: (args: string[]) => Promise<string> | string;
+};
+
 export async function shell(
   command: string,
   setHistory: (value: string) => void,
   clearHistory: () => void,
-  setCommand: React.Dispatch<React.SetStateAction<string>>, //typeScript Wrapper For setState : string
+  setCommand: React.Dispatch<React.SetStateAction<string>>,
 ) {
   const args = command.split(' ');
   args[0] = args[0].toLowerCase();
+
   if (args[0] === 'clear') {
     clearHistory();
   } else if (command === '') {
     setHistory('');
-  } else if (Object.keys(cmds).indexOf(args[0]) === -1) {
+  } else if (!(args[0] in cmds)) {
     setHistory(
-      `shell : command not found ${args[0]}. Try 'help' to get started.`,
+      `shell: command not found ${args[0]}. Try 'help' to get started.`,
     );
   } else {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const output = await (cmds as any)[args[0]](args.slice(1)); //arg[0] is a function in cmds  args.slice(1) is an arg[0] arguments IE: weather(tokyo)
+    const output = await (cmds as CommandFunctions)[args[0]](args.slice(1));
 
     setHistory(output);
   }
